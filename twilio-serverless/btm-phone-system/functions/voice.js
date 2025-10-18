dbe2561e203b43d75b0c11d1009e19072d964d3a
kinabraytan <jnebria@outlook.com>
2025-10-17 22:03:53 -0400

    Update latest build version and refactor voice function for number normalization

--- twilio-serverless/btm-phone-system/functions/voice.js
+++ twilio-serverless/btm-phone-system/functions/voice.js
@@ -17,17 +17,21 @@ exports.handler = function(context, event, callback) {
     return callback(null, response);
   }
 
-  if (event.To) {
-    const dial = twiml.dial({ callerId });
-    const to = event.To.trim();
-    if (to.startsWith('client:')) {
-      dial.client(to.replace('client:', ''));
-    } else {
-      dial.number(to);
-    }
-  } else {
-    const dial = twiml.dial();
+  const toRaw = (event.To || '').trim();
+  const normalizeNumber = (value) => (value || '').replace(/[^\d]/g, '');
+  const toNormalized = normalizeNumber(toRaw);
+  const callerIdNormalized = normalizeNumber(callerId);
+
+  if (!toNormalized || toNormalized === callerIdNormalized) {
+    const dial = twiml.dial({ callerId: event.From || callerId });
     dial.client(identity);
+  } else {
+    const dial = twiml.dial({ callerId });
+    if (toRaw.startsWith('client:')) {
+      dial.client(toRaw.replace('client:', ''));
+    } else {
+      dial.number(toRaw);
+    }
   }
 
   response.setBody(twiml.toString());
